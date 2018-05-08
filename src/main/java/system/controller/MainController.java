1a12f753e468e8377af9bc1cc621a57e4bd6b106
Alexander <79089417108@yandex.ru>
2018-05-08 21:30:50 +0700

    123

--- src/main/java/system/controller/MainController.java
+++ src/main/java/system/controller/MainController.java
@@ -1,11 +1,13 @@
 package system.controller;
 
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import system.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import system.service.MainService;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -16,49 +18,86 @@ public class MainController {
     private MainService mainService;
 
 
-    @RequestMapping(value = "/insertTest", method = RequestMethod.GET)
-    public String listBooks(){
+    //  TEST
+            @RequestMapping(value = "/insertTest", method = RequestMethod.GET)
+            public String listBooks(){
 
-        Item item = new Item("Test");
+                Item item = new Item("Test");
+                mainService.insert(item);
+
+                return "redirect:/";
+            }
+
+            @RequestMapping(value = "/getItemsByNameTest", method = RequestMethod.GET)
+            public String getItemsByName(){
+
+                List<Item> items = mainService.getItemsByName("Test");
+
+                return "redirect:/";
+
+            }
+
+            @RequestMapping(value = "/getAllItems", method = RequestMethod.GET)
+            public String getAllItems(){
+
+                List<Item> items = mainService.getAllItems();
+
+                return "redirect:/";
+
+            }
+    // ~TEST
+
+    @RequestMapping(value = "/addNew", method = RequestMethod.POST)
+    public String addNew(HttpServletRequest request){
+
+        String name = request.getParameter("name");
+        Item item = new Item(name);
         mainService.insert(item);
 
         return "redirect:/";
-    }
-
-    @RequestMapping(value = "/getItemsByNameTest", method = RequestMethod.GET)
-    public String getItemsByName(){
-
-        List<Item> items = mainService.getItemsByName("Test");
-
-        return "redirect:/";
 
     }
 
-    @RequestMapping(value = "/getAllItems", method = RequestMethod.GET)
-    public String getAllItems(){
+/*    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ModelAndView find(HttpServletRequest request){
 
+        String name = request.getParameter("name");
+        Item item = new Item(name);
+
+        List<Item> items = mainService.getItemsByName(name);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject(items);
+
+        return modelAndView;
+
+    }*/
+
+
+/*    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getAll(){
         List<Item> items = mainService.getAllItems();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject(items);
+        return modelAndView;
+    }*/
 
-        return "redirect:/";
 
-    }
+/*    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String listBooks(Model model){
 
+        model.addAttribute("item", new Item(""));
+        model.addAttribute("listItems", this.mainService.getAllItems());
 
-//    @RequestMapping(value = "/", method = RequestMethod.GET)
-//    public String listBooks(Model model){
-//
-//        model.addAttribute("item", new Item(""));
-//        model.addAttribute("listItems", this.mainService.getAllItems());
-//
-//        return "/";
-//    }
+        return "/";
+    }*/
 
-//    @RequestMapping(value = "/", method = RequestMethod.POST)
-//    public String insert(@ModelAttribute("item") Item item){
-//
-//        mainService.insert(item);
-//
-//        return "/";
-//    }
+/*    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String insert(@ModelAttribute("item") Item item){
+
+        mainService.insert(item);
+
+        return "/";
+    }*/
 
 }
--- target/test4springMVC-1.0-SNAPSHOT/WEB-INF/pages/items.jsp
+++ target/test4springMVC-1.0-SNAPSHOT/WEB-INF/pages/items.jsp
@@ -1,4 +1,4 @@
-
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
 <html>
@@ -11,6 +11,18 @@
     <h1>Items List</h1>
     <br>
 
+    <form action="/find" method="post">
+        Find:
+        <input type="text" name="name">
+        <input type="submit" title="find"/>
+    </form>
+
+
+    <form action="/addNew" method="post">
+        Add new:
+        <input type="text" name="name">
+        <input type="submit" title="Add new"/>
+    </form>
 
 
 </body>
